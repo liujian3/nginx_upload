a7a3edea8d76e9093ad10c628f64ddab2e0fc31f
liujian3 <liujian3@gmail.com>
2021-03-26 09:44:30 +0800

    Update send163.py

--- send163.py
+++ send163.py
@@ -1,23 +1,27 @@
-import sys
+import sys,os
 import smtplib
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-# 创建一个带附件的邮件实例
-message=MIMEMultipart()
-# 邮件的其他属性
-message['From'] = '<safeei@126.com>'
-message['Subject'] = Header(sys.argv[1], 'utf8').encode()
-message['To'] = '<safeei@126.com>'
-# 邮件正文内容
-attr2 = MIMEText(sys.argv[1], 'plain', 'utf-8')
-message.attach(attr2)
-#构造附件txt附件1
-attr1=MIMEText(open('/'+sys.argv[1],'rb').read(),'base64','utf-8')
-attr1["content_Type"]='application/octet-stream'
-attr1["Content-Disposition"] = 'attachment; filename="'+sys.argv[1]+'"' 
-message.attach(attr1)
-server = smtplib.SMTP('smtp.126.com', 25)
-server.login('safeei@126.com', 'GKCFSQMQYBSMRRRE')
-server.sendmail('safeei@126.com', ['safeei@126.com'],message.as_string())
-print("ok")
+def sendfile(f,path='upload'):
+    print(f)
+    message=MIMEMultipart()
+    message['From'] = '<safeei@126.com>'
+    message['Subject'] = Header(f, 'utf8').encode()
+    message['To'] = '<safeei@126.com>'
+    attr2 = MIMEText(f, 'plain', 'utf-8')
+    message.attach(attr2)
+    attr1=MIMEText(open('/'+path+'/'+f,'rb').read(),'base64','utf-8')
+    attr1["content_Type"]='application/octet-stream'
+    attr1["Content-Disposition"] = 'attachment; filename="'+f+'"' 
+    message.attach(attr1)
+    server = smtplib.SMTP('smtp.126.com', 25)
+    server.login('safeei@126.com', 'GKCFSQMQYBSMRRRE')
+    server.sendmail('safeei@126.com', ['safeei@126.com'],message.as_string())
+    print(f+'ok')
+os.system('split -b 50m '+sys.argv[1]+' safeei')
+path='upload'
+if len(sys.argv)>2:
+    path=sys.argv[2]
+for f in os.listdir('/'+path+'/'):
+    sendfile(f,path)
