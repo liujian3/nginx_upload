b03721410b15226e56770b3f20d1311a58256f63
liujian3 <liujian3@gmail.com>
2021-04-15 10:46:53 +0800

    Update send163.py

--- send163.py
+++ send163.py
@@ -1,23 +1,25 @@
-import sys,os
+mport sys,os
 import smtplib
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+u=sys.argv[2]
+p='GDGRWRYVZBMEQPVO' if u=='safermd' else 'GKCFSQMQYBSMRRRE'
 def sendfile(f):
     print(f)
     message=MIMEMultipart()
-    message['From'] = '<safeei@126.com>'
+    message['From'] = '<'+u+'@126.com>'
     message['Subject'] = Header(f, 'utf8').encode()
     message['To'] = '<safeei@126.com>'
     attr2 = MIMEText(f, 'plain', 'utf-8')
     message.attach(attr2)
     attr1=MIMEText(open('./'+f,'rb').read(),'base64','utf-8')
     attr1["content_Type"]='application/octet-stream'
-    attr1["Content-Disposition"] = 'attachment; filename="'+f+'"' 
+    attr1["Content-Disposition"] = 'attachment; filename="'+f+'"'
     message.attach(attr1)
     server = smtplib.SMTP('smtp.126.com', 25)
-    server.login('safeei@126.com', 'GKCFSQMQYBSMRRRE')
-    server.sendmail('safeei@126.com', ['safeei@126.com'],message.as_string())
+    server.login(u+'@126.com', p)
+    server.sendmail(u+'@126.com', ['safeei@126.com'],message.as_string())
     print(f+'ok')
     os.remove('./'+f)
 os.chdir('nginx_upload')
